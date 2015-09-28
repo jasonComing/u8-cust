a7a4ed1f875be9614b9440ef38f89114a10ad5b5
jasonComing <jason.ching@comingtech.com.hk>
2015-09-28 15:39:26 +0800

    Change to use v_CustSoInventoryMap

--- db/trigger/tri_PO_Podetails.sql
+++ db/trigger/tri_PO_Podetails.sql
@@ -1,12 +1,17 @@
 USE [UFDATA_003_2015]
 GO
 
-/****** Object:  Trigger [dbo].[tri_PO_Podetails]    Script Date: 2015-09-28 14:24:03 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+
+IF EXISTS (select 1 from sys.objects where object_id = object_id(N'[dbo].[tri_PO_Podetails]') and type = 'TR')
+BEGIN
+	DROP TRIGGER dbo.tri_PO_Podetails
+END
+go
 
 CREATE TRIGGER [dbo].[tri_PO_Podetails]
    ON  [dbo].[PO_Podetails]
@@ -16,16 +21,11 @@ BEGIN
 	SET NOCOUNT ON;
 
 	update b set b.cDefine29=e.cInvDefine1,b.cDefine33=f.cidefine9
-	from PO_Pomain a 
-	inner join PO_Podetails b on a.POID =b.POID  --and a.cPOID='PO15090051'
+	from PO_Podetails b
 	inner join Inserted bb on b.ID=bb.ID
-	inner join SO_SOMain c on b.SoDId=c.cSOCode
-	inner join SO_SODetails d on c.ID=d.ID
-	inner join Inventory e on d.cInvCode=e.cInvCode
+	inner join v_CustSoInventoryMap m on b.SoDId = m.cSOCode
+	inner join Inventory e on m.cInvCode=e.cInvCode
 	left join Inventory_extradefine f on e.cInvCode=f.cInvCode
-
-	
-
 END
 
 GO
