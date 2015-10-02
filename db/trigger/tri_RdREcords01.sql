b66e237a99456a05e47e3e6f611b54a133ffde65
jasonComing <jason.ching@comingtech.com.hk>
2015-10-03 00:00:21 +0800

    Update 入庫餘數 in 採購入庫單

--- db/trigger/tri_RdREcords01.sql
+++ db/trigger/tri_RdREcords01.sql
@@ -26,6 +26,14 @@ BEGIN
 	inner join v_CustSoInventoryMap m on b.isodid = m.cSOCode
 	inner join Inventory e on m.cInvCode=e.cInvCode
 	left join Inventory_extradefine f on e.cInvCode=f.cInvCode
+	
+	-- Update 入庫餘數 in 採購入庫單
+	update e
+	set e.cbdefine6 = a.iQuantity - d.iQuantity - a.fSumRefuseQuantity
+	from rdrecords01_extradefine e
+	join RdRecords01 d on d.AutoId = e.AutoId
+	join PU_ArrivalVouchs a on a.AutoId = d.iArrsId
+	join inserted i on i.AutoId = d.AutoId
 END
 
 GO
