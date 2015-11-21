1ce766392d0d1e65a10f919d9b06e72e1cd60d55
jasonComing <jasonc@comingtech.com.hk>
2015-11-21 13:10:54 +0800

    new user and new view

---
+++ db/user/f8report.sql
@@ -0,0 +1,13 @@
+create login f8report with password = '1Punch@Hero';
+go
+
+use UFDATA_001_2015
+create user f8report for login f8report;
+go
+grant select on SCHEMA::dbo to f8report
+go
+grant insert,update,delete on CustInvClassMetaData to f8report
+grant insert,update,delete on CustInvSpec to f8report
+grant insert,update,delete on CustInvSpecDetail to f8report
+grant insert,update,delete on CustSpecProperty to f8report
+grant insert,update,delete on CustSpecPropertyOption to f8report
