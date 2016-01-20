47d24156816af204ae7d73b6d441ae71de920944
huangzuozhan <jams123456>
2016-01-20 19:25:02 +0800

    change trigger ,use update  from join

--- db/trigger/tri_FetchRemarkValueAndUpdate.sql
+++ db/trigger/tri_FetchRemarkValueAndUpdate.sql
@@ -8,26 +8,18 @@ Create trigger tri_FetchRemarkValueAndUpdate
 on  CustInvSpec
 for update
 as 
- begin  
-	Set nocount on
-  declare @cinvCode varchar(20),@Remarktext nvarchar(1000),@varsion varchar(20)
+begin  
+	declare @cinvCode varchar(20)
+	declare @Remarktext nvarchar(1000)
+	declare @varsion varchar(20)
 	if update(status) 
-		begin
-	 		select inserted.cinvcode,inserted.version,CustInvSpecDetail.SpecValue  into #temptb from inserted  
-				left join CustInvSpecDetail on inserted.cinvcode=CustInvSpecDetail.cinvcode
-			where inserted.cinvcode=CustInvSpecDetail.cinvcode and  CustInvSpecDetail.Propertyid=102
-				and inserted.Version= CustInvSpecDetail.version and inserted.status='1'
-	--inserted.cinvcode='602_000010' and
-	--select CustInvSpec.cinvcode,CustInvSpec.version,CustInvSpecDetail.SpecValue   from CustInvSpec  
-	--left join CustInvSpecDetail on CustInvSpec.cinvcode=CustInvSpecDetail.cinvcode
-	--where CustInvSpec.cinvcode=CustInvSpecDetail.cinvcode and CustInvSpecDetail.Propertyid=102
-	--and CustInvSpec.Version= CustInvSpecDetail.version and CustInvSpec.status='1' order by cinvcode 
-	--select * from #temptb
-
-			update Inventory_extradefine 
-				set cidefine16=(select top 1 SpecValue from  #temptb where #temptb.cinvcode=Inventory_extradefine.cinvcode  ) 
-			where --( cidefine16 is  null OR cidefine16 ='') and
-				Inventory_extradefine.cinvcode in (select cinvcode  from  #temptb )
-		end
-	Set nocount off
+	begin
+		update  ie 
+		set ie.cidefine16 =c.SpecValue
+		from  Inventory_extradefine ie
+		inner join inserted d on  ie.cinvcode = d.cinvcode
+		left join CustInvSpecDetail c on (d.cinvcode = c.cinvcode and d.version = c.version)
+		where d.status = 1 and 
+		c.Propertyid = 102  
+	end
 end 
--- db/trigger/tri_PoVersonAutoInsert.sql
+++ db/trigger/tri_PoVersonAutoInsert.sql
@@ -1,40 +1,19 @@
 CREATE  trigger tri_PoVersonAutoInsert
 /*
-功能：自动补上采购单据表身料件的规格版本号
+功能：自动补上采购单据表身料件的规格版本号,
+	　版本号取新建采购单据时，系统已经审核的版本号．		
 作者：Jams
 */
 on PO_Podetails_ExtraDefine
-   for insert
+	for insert
 as
 begin
-	--Set nocount on
-	--declare  @id int,@cInvcode varchar(20),@cVerson varchar(10)
-	--declare my_cursor  cursor for
-	--	--Select  id,cInvCode From inserted   order by id desc 
-	--	Select ID from  inserted
-	--open my_cursor
-	--fetch next from my_cursor into @id
-
-	--while @@FETCH_STATUS = 0 
-	--begin
-	--	select @cInvcode=cinvCode from PO_Podetails  where id=@id
-	--	select top 1 @cVerson=Version from CustInvSpec where cInvCode =@cInvcode  and Status = 1
- --       update PO_Podetails_ExtraDefine set cbdefine27=@cVerson where  id=@id and ( cbdefine27 is  null OR cbdefine27 ='')
-	--	--raisError(@cVerson,16,11)
-	--	fetch next from my_cursor into @id
-	--end
-	
-	--close my_cursor
-	--deallocate my_cursor
-	--set nocount off
-	set nocount on
-		select ta.id,tb.cinvCode,tc.version into #temptb from inserted ta
-			left join PO_Podetails tb on ta.id=tb.id
-			left join CustInvSpec tc on tb.cInvCode=tc.cInvCode
-			where --tb.cInvCode='605_000111'and 
-			tc.Status = 1
-		update PO_Podetails_ExtraDefine set cbdefine27=(select top 1 version from  #temptb where PO_Podetails_ExtraDefine.id=#temptb.id ) 
-		where  cbdefine27 is  null OR cbdefine27 =''
-
-	 set nocount off
+	update e  
+	set cbdefine27 = isnull(c.Version,'')
+	from PO_Podetails_ExtraDefine e
+	inner join inserted i on e.ID = i.ID
+	inner join PO_Podetails s on e.id = s.id
+	inner join CustInvSpec c on s.cInvCode = c.cInvCode
+	where c.Status = 1 and
+	(e.cbdefine27 is null OR e.cbdefine27 = '')
 end
