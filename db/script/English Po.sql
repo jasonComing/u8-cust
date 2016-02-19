1e317da0bebf7d0c5fcdf9ac33e74ffc6dc28dd8
jasonComing <jasonc@comingtech.com.hk>
2016-02-19 17:08:51 +0800

    add

---
+++ db/script/English Po.sql
@@ -0,0 +1,13 @@
+Insert into CustSpecProperty (PropertyId, Name, PropertyType) values (212, 'Size, Length & Thickness', 6)
+Insert into CustSpecProperty (PropertyId, Name, PropertyType) values (213, 'Construction', 6)
+Insert into CustSpecProperty (PropertyId, Name, PropertyType) values (214, 'Top Material & Color Code', 6)
+Insert into CustSpecProperty (PropertyId, Name, PropertyType) values (215, 'Lining', 6)
+Insert into CustSpecProperty (PropertyId, Name, PropertyType) values (216, 'Buckle', 6)
+Insert into CustSpecProperty (PropertyId, Name, PropertyType) values (217, 'Accessories', 6)
+
+Insert into CustInvClassMetaData (cInvCCode, PropertyId, SortOrder) values ('619', 212, 11)
+Insert into CustInvClassMetaData (cInvCCode, PropertyId, SortOrder) values ('619', 213, 12)
+Insert into CustInvClassMetaData (cInvCCode, PropertyId, SortOrder) values ('619', 214, 13)
+Insert into CustInvClassMetaData (cInvCCode, PropertyId, SortOrder) values ('619', 215, 14)
+Insert into CustInvClassMetaData (cInvCCode, PropertyId, SortOrder) values ('619', 216, 15)
+Insert into CustInvClassMetaData (cInvCCode, PropertyId, SortOrder) values ('619', 217, 16)
