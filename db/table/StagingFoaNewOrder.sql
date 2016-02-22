7f970381e7cf9072f6a757362d1938a865411e5e
jasonComing <jasonc@comingtech.com.hk>
2016-02-22 17:34:29 +0800

    add

--- db/table/StagingFoaNewOrder.sql
+++ db/table/StagingFoaNewOrder.sql
@@ -4,29 +4,27 @@ Create table StagingFoaNewOrder
 (
 Id   int identity,
 Date	datetime not null,
+OrderStatus varchar(50) not null,
+TypeOfChange varchar(50) not null,
 PoNo	varchar(50) null,
-PoLineNo	varchar(10) null,
-CustSoNo	varchar(50) null,
-CustSoLineNo	varchar(10) null,
-OrderDate	datetime null,
+OrderLine	varchar(10) null,
+SoNo	varchar(50) null,
 Style	varchar(50) null,
-ShipTo	varchar(20) null,
-PoLineQty	int null,
-ShipWindow	datetime null,
-ReorderLT	varchar(50) null,
-Factory	varchar(50) null,
-OrderType	varchar(20) null,
-LineType	varchar(20) null,
-Collection	varchar(20) null,
-BrandCode	varchar(20) null,
 Brand		varchar(20) null,
-Dome		varchar(20) null,
-BuyPrice	decimal null,
-PoRemarks	varchar(5000) null,
-PoLineRemarks	varchar(5000) null,
-ReqDelvDate	datetime null
+UnitPrice	decimal null,
+PoOrderQty	int null,
+OrderType  varchar(50) null,
+LineType	varchar(20) null,
+Season  varchar(50) null,
+CreateDate  datetime null,
+ShipWindow	datetime null,
+ShipTo	varchar(20) null,
+ShippingAgent varchar(50) null,
+Factory  varchar(50) null
 )
 go
 create index idx_FoaNewOrder on StagingFoaNewOrder(Date)
 go
 grant select,insert,update,delete on StagingFoaNewOrder to f8report
+
+insert into StagingFoaNewOrder (Date, OrderStatus, TypeOfChange, PoNo, OrderLine, SoNo, Style, Brand, UnitPrice, PoOrderQty, LineType, Season, CreateDate, ShipWindow, ShipTo, ShippingAgent, Factory) values ()
