a622336df228eceaad81a5a6f0f16229d09b8d79
jasonComing <jasonc@comingtech.com.hk>
2016-03-12 11:17:12 +0800

    add

--- db/table/FoaShipMethodConfig.sql
+++ db/table/FoaShipMethodConfig.sql
@@ -10,8 +10,11 @@ go
 grant select on CustFoaShipMethodConfig to f8report
 go
 
-insert CustFoaShipMethodConfig (Regex, ShipMethod, Sort)
-select Regex, ShipMethod, Sort
-from Cust001..FoaShipMethodConfig
+insert CustFoaShipMethodConfig (Regex, ShipMethod, Sort) values ('DHL.*', '空运', 10)
+insert CustFoaShipMethodConfig (Regex, ShipMethod, Sort) values ('LOCAL.*', 'LOCAL(本地)', 20)
+insert CustFoaShipMethodConfig (Regex, ShipMethod, Sort) values ('UPS.*', '空运', 30)
+insert CustFoaShipMethodConfig (Regex, ShipMethod, Sort) values ('FEDEX.*', '空运', 40)
+insert CustFoaShipMethodConfig (Regex, ShipMethod, Sort) values ('.+-A.*', '空运', 50)
+insert CustFoaShipMethodConfig (Regex, ShipMethod, Sort) values ('.+-S.*', '海运', 60)
 
 go
