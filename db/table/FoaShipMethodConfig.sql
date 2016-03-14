4a4ab7b651ea309d359b5806314cee98eb97e4fe
jasonComing <jasonc@comingtech.com.hk>
2016-03-14 11:39:22 +0800

    modify CustFoaShipMethodConfig

--- db/table/FoaShipMethodConfig.sql
+++ db/table/FoaShipMethodConfig.sql
@@ -2,7 +2,7 @@ create table CustFoaShipMethodConfig
 (
 	Id int identity,
 	Regex varchar(200) not null,
-	ShipMethod varchar(50) not null,
+	ShipMethod int not null,
 	ShipMethodDescription varchar(50) not null,
 	Sort int not null,
 	LastModified datetime default getdate() not null
@@ -11,11 +11,11 @@ go
 grant select on CustFoaShipMethodConfig to f8report
 go
 
-insert CustFoaShipMethodConfig (Regex, ShipMethod, ShipMethodDescription, Sort) values ('DHL.*', 'Air', '空运', 10)
-insert CustFoaShipMethodConfig (Regex, ShipMethod, ShipMethodDescription, Sort) values ('LOCAL.*', 'Local', 'LOCAL(本地)', 20)
-insert CustFoaShipMethodConfig (Regex, ShipMethod, ShipMethodDescription, Sort) values ('UPS.*', 'Air', '空运', 30)
-insert CustFoaShipMethodConfig (Regex, ShipMethod, ShipMethodDescription, Sort) values ('FEDEX.*', 'Air', '空运', 40)
-insert CustFoaShipMethodConfig (Regex, ShipMethod, ShipMethodDescription, Sort) values ('.+-A.*', 'Air', '空运', 50)
-insert CustFoaShipMethodConfig (Regex, ShipMethod, ShipMethodDescription, Sort) values ('.+-S.*', 'Sea', '海运', 60)
+insert CustFoaShipMethodConfig (Regex, ShipMethod, ShipMethodDescription, Sort) values ('DHL.*', 2, '空运', 10)
+insert CustFoaShipMethodConfig (Regex, ShipMethod, ShipMethodDescription, Sort) values ('LOCAL.*', 1, 'LOCAL(本地)', 20)
+insert CustFoaShipMethodConfig (Regex, ShipMethod, ShipMethodDescription, Sort) values ('UPS.*', 2, '空运', 30)
+insert CustFoaShipMethodConfig (Regex, ShipMethod, ShipMethodDescription, Sort) values ('FEDEX.*', 2, '空运', 40)
+insert CustFoaShipMethodConfig (Regex, ShipMethod, ShipMethodDescription, Sort) values ('.+-A.*', 2, '空运', 50)
+insert CustFoaShipMethodConfig (Regex, ShipMethod, ShipMethodDescription, Sort) values ('.+-S.*', 3, '海运', 60)
 
 go
