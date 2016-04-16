6eac96deb006ec616711ae053ce0d7f43356d2f5
huangzuozhan <jams123456>
2016-04-16 11:46:29 +0800

    jobbug

--- db/view/v_CustSoInventoryMap.sql
+++ db/view/v_CustSoInventoryMap.sql
@@ -1,4 +1,4 @@
-ALTER VIEW dbo.v_CustSoInventoryMap
+alter VIEW dbo.v_CustSoInventoryMap
 AS
 	-- Display SO No, Inventory Code
 	select c.cSOCode, d.cInvCode
@@ -6,11 +6,11 @@ AS
 	inner join SO_SODetails d on d.AutoID = (
 		select top 1 AutoID 
 		from SO_SODetails 
+		join  SO_SODetails_extradefine sx on  SO_SODetails.iSOsID=sx.iSOsID
 		where SO_SODetails.ID = c.ID
-		and cInvCode != 'D_GENERAL')
+		and cInvCode != 'D_GENERAL'
+      and sx.cbdefine30 > 0 )
 	where len(c.csocode) = 10
 	and c.csocode not like '%/%'
 	and c.csocode not like '%\%'
 	and c.csocode not like '%-%'
-
-GO
