dd1a84865c46ccf26237999fe959d1f818401492
huangzuozhan <jams123456>
2016-04-22 11:43:52 +0800

    销售订单实际数量修改

--- db/trigger/tri_UpdateSoDetails_setActualQty.sql
+++ db/trigger/tri_UpdateSoDetails_setActualQty.sql
@@ -5,45 +5,28 @@ alter  trigger [dbo].[tri_UpdateSoDetails_setActualQty] ON [dbo].[SO_SODetails]
 */ 
 FOR Update,insert
 AS  
-set nocount on  
-
---declare @quantity int
---select @quantity = case
---  when id.cscloser is null then isnull(id.iQuantity,0)
---  else isnull(id.iKPQuantity, 0)
---  end
---from inserted id
-
---update SO_SODetails_ExtraDefine
---set cbdefine30 = @quantity,
---  cbdefine31=@quantity * i.iUnitPrice  
---from SO_SODetails_ExtraDefine s
---inner join inserted i on s.isosid = i.isosid
-
+	set nocount on  
   --批量修改,查找出需修改的行与数据(行号,订单量,开票量,单价,是否关闭)
-	select isosid,iQuantity,iKPQuantity,iUnitPrice,cscloser
-	into #temp
-	from inserted
-
 	update SO_SODetails_ExtraDefine
-	set cbdefine30 =isnull(i.iQuantity,0) ,
-	cbdefine31=isnull(i.iQuantity,0) * i.iUnitPrice  
+	set cbdefine30 =(
+			case when cSCloser is null then	isnull(i.iQuantity,0)
+			else 
+				isnull(i.iKPQuantity,0)
+			end
+			)
 	from SO_SODetails_ExtraDefine s
-	inner join #temp i on s.isosid = i.isosid
-	where cscloser is null
-
+	inner join inserted i on s.isosid = i.isosid
+ 
 	update SO_SODetails_ExtraDefine
-	set cbdefine30 =isnull(i.iKPQuantity,0),
-	cbdefine31=isnull(i.iKPQuantity,0) * i.iUnitPrice  
+	set cbdefine31=cbdefine30 * i.iUnitPrice  
 	from SO_SODetails_ExtraDefine s
-	inner join #temp i on s.isosid = i.isosid
-	where cscloser is not null
+	inner join inserted i on s.isosid = i.isosid
 
-set nocount off
-
+	set nocount off
 
 
-select * from [SO_SODetails] where cSOCode='test0422'
 
-update [SO_SODetails] set iKPQuantity=50 where autoID=15073
-15073
+--select * from [SO_SODetails] where cSOCode='test0422'
+
+--update [SO_SODetails] set iKPQuantity=50 where autoID=15073
+--15073
