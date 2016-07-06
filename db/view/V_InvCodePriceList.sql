14a1f4efc07d244c1afd9b3e13850b49917e648c
huangzuozhan <jams123456>
2016-07-06 08:12:00 +0800

    pofilldate cha0706

--- db/view/V_InvCodePriceList.sql
+++ db/view/V_InvCodePriceList.sql
@@ -1,18 +1,13 @@
-create view  V_InvCodePriceList
+alter view  V_InvCodePriceList
 /*
 功能：取料件最小单价
 	（在历史PO单价中与最高进价控制两者之间选取）
 */
 as
-	  select tbtemp.cInvCode,min(tbtemp.MinPrice) as 'MinPrice'
-	  from (select Inventory.cInvCode,isnull(Inventory.iInvMPCost,0) as 'MinPrice'  from Inventory
-				union
-				select PO_Podetails.cInvCode, PO_Podetails.iUnitPrice as 'MinPrice' from PO_Podetails
-				where  PO_Podetails.ID in (
-								select MAX(po.id)
-								from PO_Podetails as po
-								where po.iUnitPrice != 0
-								group by po.cInvCode
-								)
-			  ) as tbtemp
-	  group by tbtemp.cInvCode
+	select d.cInvCode, d.iUnitPrice as 'UnitPrice', i.iInvMPCost, iif(d.iUnitPrice > i.iInvMPCost, i.iInvMPCost, d.iUnitPrice) as 'MinPrice'
+	from PO_Podetails d
+	join (select MAX(po.id) as Id
+	from PO_Podetails as po
+	where po.iUnitPrice != 0
+	group by po.cInvCode) t on t.Id = d.id
+	join Inventory i on i.cInvCode = d.cInvCode
