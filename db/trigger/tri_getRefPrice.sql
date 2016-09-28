60c023bf2e36a18dcb7c47bcb9cafbfad4432e04
jasonComing <jasonc@comingtech.com.hk>
2016-09-28 16:29:27 +0800

    refprice

--- db/trigger/tri_getRefPrice.sql
+++ db/trigger/tri_getRefPrice.sql
@@ -1,31 +1,31 @@
-CREATE trigger tri_getRefPrice
+ALTER trigger [dbo].[tri_getRefPrice]
 /*
  功能：创建采购订单时，取出料件最近一次的历史
- 采购单价
+ 采购单价做為參考單價
  Auth:Jams
  调用：建立采购单时，自动触发 
 */
-on PO_Podetails_extradefine
+on [dbo].[PO_Podetails_extradefine]
    for insert,update
 as
 begin
 	Set nocount on
+
 	update PO_Podetails_extradefine
 	set cbdefine32=(
 						select top 1 isnull(dd.iunitprice,0)
 						from inserted a
-						join  PO_Podetails b  on  a.id=b.id
+						join  PO_Podetails b on a.id=b.id
 						join PO_Pomain c on b.poid=c.poid
 						left join PO_PODetails dd on dd.cInvCode = b.cInvCode
 						left join PO_POMain dm on dm.poid = dd.poid
 						where dd.iUnitPrice > 0  
 						and dm.cVenCode = c.cVenCode
 						and b.id != dd.id
+						and a.id=aa.ID
 						order by dd.id desc
 						)
-	 from PO_Podetails_extradefine join inserted aa on  PO_Podetails_extradefine.id=aa.id
-	 where PO_Podetails_extradefine.id=aa.id
+	 from PO_Podetails_extradefine
+	 join inserted aa on PO_Podetails_extradefine.id=aa.id
 	set nocount off
-end 
-
-
+end
