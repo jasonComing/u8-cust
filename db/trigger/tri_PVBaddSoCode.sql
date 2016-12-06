d94fbcbaf44c129b4f352ec60ebbac79c7ef08e4
huangzuozhan <jams123456>
2016-12-06 11:06:35 +0800

    bill

--- db/trigger/tri_PVBaddSoCode.sql
+++ db/trigger/tri_PVBaddSoCode.sql
@@ -9,16 +9,15 @@ as
 begin
 	set nocount on
 
-	update  pe 	set  cbdefine3 =temp.csocode
+	update pe set cbdefine3 = temp.csocode
 	from PurBillVouchs_extradefine pe
 	left join (
 		select i.cbdefine3,po.csocode,p.ID
-		from inserted  i
-		left join PurBillVouchs p on  i.id = p.id
-		left join PO_Podetails po on  po.id = p.iPOsID
-		) temp  on  temp.ID = pe.ID
-	where pe.cbdefine3  is null or  pe.cbdefine3 = ''
+		from inserted i
+		left join PurBillVouchs p on i.id = p.id
+		left join PO_Podetails po on po.id = p.iPOsID
+		) temp on temp.ID = pe.ID
+	where pe.cbdefine3 is null or pe.cbdefine3 = ''
 
 	set nocount off
-
 end
